033795a26de05d0d4caba1fc7ece5259900ddb93
Shane Geary <stg4462@comcast.net>
2020-02-07 13:59:15 +0000

    Automatically backed up by Learn

--- spec/transfer_spec.rb
+++ spec/transfer_spec.rb
@@ -98,7 +98,8 @@ describe 'Transfer' do
       closed_account_transfer = Transfer.new(amanda, terrance, 50)
       expect(closed_account_transfer.execute_transaction).to eq("Transaction rejected. Please check your account balance.")
       expect(closed_account_transfer.status).to eq("rejected")
-      
+      expect(amanda).to receive(:valid?).and_return(true)
+        expect(avi).to receive(:valid?).and_return(true)
 
       expect(bad_transfer.execute_transaction).to eq("Transaction rejected. Please check your account balance.")
       expect(bad_transfer.status).to eq("rejected")
