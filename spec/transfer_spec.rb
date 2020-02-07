7a6fdbc5e815479955845f4389298fc623727ca2
Shane Geary <stg4462@comcast.net>
2020-02-07 14:00:54 +0000

    Automatically backed up by Learn

--- spec/transfer_spec.rb
+++ spec/transfer_spec.rb
@@ -118,7 +118,28 @@ describe 'Transfer' do
       expect(amanda.balance).to eq(1000)
       expect(transfer.status).to eq("reversed")
     end
-    
+    describe '#execute_transaction' do
+      it "can execute a successful transaction between two accounts" do
+        transfer.execute_transaction
+        expect(amanda.balance).to eq(950)
+        expect(avi.balance).to eq(1050)
+        expect(transfer.status).to eq("complete")
+      end
+
+      it "each transfer can only happen once" do
+        transfer.execute_transaction
+        expect(amanda.balance).to eq(950)
+        expect(avi.balance).to eq(1050)
+        expect(transfer.status).to eq("complete")
+        transfer.execute_transaction
+        expect(amanda.balance).to eq(950)
+        expect(avi.balance).to eq(1050)
+      end
+
+      it "rejects a transfer if the sender doesn't have a valid account" do
+        expect(bad_transfer.execute_transaction).to eq("Transaction rejected. Please check your account balance.")
+        expect(bad_transfer.status).to eq("rejected")
+      end
 
     it "it can only reverse executed transfers" do
       transfer.reverse_transfer
