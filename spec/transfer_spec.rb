b28174aedef5f05e04ce2e3ba905987a742da70a
Shane Geary <stg4462@comcast.net>
2020-02-07 13:58:42 +0000

    Automatically backed up by Learn

--- spec/transfer_spec.rb
+++ spec/transfer_spec.rb
@@ -90,7 +90,8 @@ describe 'Transfer' do
       expect(amanda.balance).to eq(950)
       expect(avi.balance).to eq(1050)
     end
-    
+    it "calls on the sender and reciever's #valid? methods" do
+        transfer_class = File.read("lib/transfer.rb")
 
     it "rejects a transfer if the sender does not have enough funds (does not have a valid account)" do
       terrance.close_account
